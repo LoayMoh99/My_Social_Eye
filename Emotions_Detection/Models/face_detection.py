39334c4816ad4ddc55caffae8c782fdc48179436
Rasco101 <ms.elhosan@gmail.com>
2022-03-03 23:13:46 +0200

    Readding emotion_detection after organization

--- Emotions_Detection/Models/face_detection.py
+++ Emotions_Detection/Models/face_detection.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy as np
-# from scipy.signal import convolve2d
 import skimage.io as io
 import matplotlib.pyplot as plt
 
 FRAME_COLOR = (255,0,0)
 
-def get_faces(img, color=FRAME_COLOR, is_dir=True):
+def get_faces_from_image(img, color=FRAME_COLOR, is_dir=True):
+    '''
+    Returns The coordinates of the detected faces in the image in a List opbject
+    args:
+        img: nd_array, color: Frame_color (depricated), is_dir: if passed img is a directory
+    '''
     face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
     imgg = None
     if is_dir:
