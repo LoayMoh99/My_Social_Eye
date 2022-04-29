0ef105b108154eb840f26616c48aba3f9cc893e5
Loay Mohamed <loaymohamed1999@gmail.com>
2022-04-29 02:32:57 +0200

    add control unit

--- Managment_Module/face_data_structure.py
+++ Managment_Module/face_data_structure.py
@@ -1,6 +1,12 @@
 class FaceData:
     def __init__(self, face_position=None, isMasked=None, emotion=None, mouthState=None):
         self.face_position = face_position
+        self.face_area = self.getFaceArea(face_position)
         self.isMasked = isMasked
         self.emotion = emotion
         self.mouthState = mouthState
+
+    # TODO calculate the area from the face position
+    def getFaceArea(self, face_position):
+        area = 0
+        return area
